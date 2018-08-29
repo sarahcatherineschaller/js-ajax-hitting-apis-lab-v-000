76d8340f50771b93e444a6b7d3eafa75f059dbec
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-29 00:44:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ function getRepositories() {
 }
 
 function displayRepositories() {
-}
+  const repos = JSON.parse(this.responseText);
+  const repoList = repos.map(function(repo) {
+    const listUsername = repo.owner.login
+    const listRepoName = repo.name
+    return (`
+      <ul>
+        <li>
+          <h2>${repo.name}</h2>
+          <a href="${repo.html_url}">${repo.html_url}</a><br>
+          <a href="#" onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" onclick="getBranches(this)">Get Branches</a><br>
+        </li>
+      </ul>`
+    );
+  }).join("")
+  document.getElementById("repositories").innerHTML = repoList
+};
 
 function getCommits() {
 }
